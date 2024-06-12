6cf2065bfa2c7609b5389f85bc971c58bc4b0196
shakilkhan80 <shakilkhan81198@gmail.com>
2024-06-12 23:32:46 +0600

    nav bar added

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Page </title>
-</head>
-<body>
+<x-layout>
+    <x-slot:heading>Contact Page</x-slot:heading>
     <h1>Hello from the contact page!!</h1>
-</body>
-</html>
+</x-layout>
