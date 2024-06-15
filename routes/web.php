e00304d6e3bd27be50b07ca70c5562795d3119f7
shakilkhan80 <shakilkhan81198@gmail.com>
2024-06-15 23:01:49 +0600

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/jobs/{id}', function ($id) {
     ];
 
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] === $id);
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
     return view('job', [
         'job' => $job
