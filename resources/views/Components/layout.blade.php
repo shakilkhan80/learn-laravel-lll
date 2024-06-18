73a93dd928020ef22eeda19a28e06f2573778418
Shakil Khan <131842581+shakilkhan80@users.noreply.github.com>
2024-06-18 16:43:08 +0600

    layout edited

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -87,7 +87,6 @@
             <!-- Mobile menu, show/hide based on menu state. -->
             <div class="md:hidden" id="mobile-menu">
                 <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                     <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
                         aria-current="page">Dashboard</a>
                     <a href="#"
