07332a8decea21d0454b61f6931b3af659bcbf62
shakilkhan80 <shakilkhan81198@gmail.com>
2024-06-21 09:18:18 +0600

    make model done 9

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     protected $table = 'job_listings';
-    //database methods
+    protected $fillable = ['title', 'salary'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
 use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
+
 Route::get('/jobs', function () {
 
     return view('jobs', ['jobs' => Job::all()]);
 });
+
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
