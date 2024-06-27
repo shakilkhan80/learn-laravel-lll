533457b7fc4bfec83666a61b4644257e7ebf1f06
shakilkhan80 <shakilkhan81198@gmail.com>
2024-06-27 22:34:35 +0600

    data base seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Job;
 
 class DatabaseSeeder extends Seeder
 {
