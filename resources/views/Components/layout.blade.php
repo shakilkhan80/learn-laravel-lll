3ec0fefc7d5c2bcbc0c3aca6b910b239f47a477a
shakilkhan80 <shakilkhan81198@gmail.com>
2024-06-28 16:47:33 +0600

    form validation test 17

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -135,8 +135,9 @@
         </nav>
 
         <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <div class="flex justify-between mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+                <x-button href="jobs/create">Create Job</x-button>
             </div>
         </header>
         <main>
--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,8 +16,12 @@
 
                                 <input type="text" name="title" id="title" autocomplete="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Job Title">
+                                    placeholder="Job Title" required>
+
                             </div>
+                            @error('title')
+                                <p class="text-red-500 text-sm italic mt-2">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                     <div class="sm:col-span-4">
@@ -27,11 +31,22 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="salary" id="salary" autocomplete="salary"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="$50,000 per year">
+                                    placeholder="$50,000 per year" required>
                             </div>
+                            @error('salary')
+                                <p class="text-red-500 text-sm italic mt-2">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
+                {{-- @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-sm italic mt-4">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+
+                @endif --}}
             </div>
         </div>
 
