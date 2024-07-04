c703f23c15b1f5d4f48b61a47901b4e3463e0593
shakilkhan80 <shakilkhan81198@gmail.com>
2024-07-04 22:22:09 +0600

    form edit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,6 @@
         @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <x-form-field class="sm:col-span-4">
                         <x-form-label for="first_name">First Name</x-form-label>
