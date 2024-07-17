f6017cb6d0a5acb6b83f9b67357f383324c79cc9
Shakil Khan <131842581+shakilkhan80@users.noreply.github.com>
2024-07-17 21:53:04 +0600

    layout changes

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -119,7 +119,6 @@
                 </div>
             </div>
         </nav>
-
         <header class="bg-white shadow">
             <div class="flex justify-between mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
