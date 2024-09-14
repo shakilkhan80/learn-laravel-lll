3cfa205f8709b2a3744e63af7669bb613795b86c
Shakil Khan <131842581+shakilkhan80@users.noreply.github.com>
2024-09-15 03:39:55 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,7 @@ use App\Http\Controllers\JobController;
 //     return new \App\Mail\JobPosted();
 // });// Route::get('test', function () {
 //     return new \App\Mail\JobPosted();
-// });// Route::get('test', function () {
-//     return new \App\Mail\JobPosted();
-// });
+// });/
 
 Route::get("/test", function () {
     $job = Job::first();
