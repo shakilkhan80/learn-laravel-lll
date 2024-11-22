aa4459b49d507a99373bfa7b0d8a0055f68767bc
Shakil Khan <131842581+shakilkhan80@users.noreply.github.com>
2024-11-22 16:40:33 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,171 @@ use App\Http\Controllers\JobController;
 // Route::get('test', function () {
 //     return new \App\Mail\JobPosted();
 // });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
 
 
 
