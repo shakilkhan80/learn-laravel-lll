7e7293252837710490b629e269ade7ce5353a8b6
Shakil Khan <131842581+shakilkhan80@users.noreply.github.com>
2025-03-07 14:10:03 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,111 @@ use App\Http\Controllers\JobController;
 // Route::get('test', function () {
 //     return new \App\Mail\JobPosted();
 // });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
+// Route::get('test', function () {
+//     return new \App\Mail\JobPosted();
+// });
 
 Route::get("/test", function () {
     $job = Job::first();
